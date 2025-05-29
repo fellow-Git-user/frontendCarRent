449915b2d85bd89c0263da0a879267e1a4fa1426
fellow-Git-user <martynas.nadeika@gmail.com>
2025-05-29 22:54:23 +0300

    Configure API_URL via process.env and add @types/node

--- src/utils/API_URL.ts
+++ src/utils/API_URL.ts
@@ -1,4 +1,17 @@
-export const API_URL =  "http://localhost:3005/api"
+export const API_URL: string = (() => {
+  if (typeof process.env.API_URL !== 'undefined') {
+    return process.env.API_URL as string;
+  }
+
+  if (process.env.NODE_ENV === 'development') {
+    console.warn('API_URL environment variable not set. Falling back to localhost for development.');
+    return 'http://localhost:3005/api';
+  }
+
+  throw new Error('API_URL is not defined in production environment!');
+})();
+
+// export const API_URL =  "https://backendcarrent.onrender.com/api"
 
 // https://backendcarrent.onrender.com
 // https://backendcarrent.onrender.com/api
