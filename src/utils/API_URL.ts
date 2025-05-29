70e21237bdaef63837662d8f04cf3ef38551073d
fellow-Git-user <martynas.nadeika@gmail.com>
2025-05-29 23:22:31 +0300

    Configure API_URL using Vite's import.meta.env.VITE_API_URL

--- src/utils/API_URL.ts
+++ src/utils/API_URL.ts
@@ -1,14 +1,14 @@
 export const API_URL: string = (() => {
-  if (typeof process.env.API_URL !== 'undefined') {
-    return process.env.API_URL as string;
+  if (typeof import.meta.env.VITE_API_URL !== 'undefined') {
+    return import.meta.env.VITE_API_URL as string;
   }
 
-  if (process.env.NODE_ENV === 'development') {
-    console.warn('API_URL environment variable not set. Falling back to localhost for development.');
+  if (import.meta.env.DEV) {
+    console.warn('VITE_API_URL environment variable not set. Falling back to localhost for development.');
     return 'http://localhost:3005/api';
   }
 
-  throw new Error('API_URL is not defined in production environment!');
+  throw new Error('VITE_API_URL is not defined in production environment!');
 })();
 
 // export const API_URL =  "https://backendcarrent.onrender.com/api"
